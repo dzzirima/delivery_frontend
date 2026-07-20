8026c9581c03238c29d0f287a43cce9b2dd67b0c
David T Zirima <dzzirima@gmail.com>
2026-07-20 12:35:52 +0200

    feat: working on new things

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 // AUTO-GENERATED — do not edit by hand. Edit .env instead.
 export const environment = {
   production: false,
-  apiUrl:     'http://localhost:4001/api/v1',
+  apiUrl:     'https://api.fantracker.net/delivery/api/v1',
   wsUrl:      'http://localhost:4001',
   googleApiKey: 'AIzaSyA_XGiWrA7Y7BevlxMKd27qAT0SuVFU2ZE',
 };
